75a55fbd3e26df95c1e60f7349a10fa3fd703c4f
Sophie LaBelle <sophie.r.labelle@gmail.com>
2023-03-01 14:09:36 -0800

    Create Data Files to be used for testing the customer, bookings and room classes

---
+++ src/data/bookingsData.js
@@ -0,0 +1,25 @@
+const bookingsData = [
+  {
+    "id": "5fwrgu4i7k55hl6sz",
+    "userID": 9,
+    "date": "2022/04/22",
+    "roomNumber": 15,
+
+  },
+  {
+    "id": "5fwrgu4i7k55hl6t5",
+    "userID": 43,
+    "date": "2022/01/24",
+    "roomNumber": 24,
+
+  },
+  {
+    "id": "5fwrgu4i7k55hl6t6",
+    "userID": 13,
+    "date": "2022/01/10",
+    "roomNumber": 12,
+
+  },
+]
+
+export default bookingsData;
