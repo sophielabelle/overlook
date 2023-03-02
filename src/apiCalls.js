6697320dbdbba5464668513352efaa18e144a104
Sophie LaBelle <sophie.r.labelle@gmail.com>
2023-03-02 15:56:50 -0800

    Create a reusable function for fething api calls

---
+++ src/apiCalls.js
@@ -0,0 +1,7 @@
+const fetchData = (data) => {
+  return fetch(`http://localhost:3001/api/v1/${data}`)
+  .then(res => res.json())
+  .catch(error => console.log(`Issue at: ${error}`))
+};
+
+export default fetchData;
