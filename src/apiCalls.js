fa18db566ff409f4b349aff01b60176c56246695
Sophie LaBelle <sophie.r.labelle@gmail.com>
2023-03-03 11:48:47 -0800

    Removing api call changes from this branch

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -2,27 +2,4 @@ const fetchData = (data) => {
   return fetch(`http://localhost:3001/api/v1/${data}`)
   .then(res => res.json())
   .catch(error => console.log(`Issue at: ${error}`))
-};
-
-export const promises = () => {
-  const customersData = 
-  return Promise.all([fetchData('customers'), fetchData('rooms'), fetchData('bookings')])
-}
-
-export const postData = (saveData) => {
-  fetch(`http://localhost:3001/api/v1/bookings`, {
-    method: 'POST',
-    body: JSON.stringify(savedRecipes),
-    headers: {
-      'Content-Type': 'application/json'
-    }  
-  })
-  .then(response => {
-    if(!response.ok) {
-      throw new Error(response.status);
-    }
-    return response.json();
-  })
-  .then(data = console.log(data))
-  .catch(error => console.log(`Issue at: ${error}`));
 }
