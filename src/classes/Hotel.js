1cc97a95cf57a2e57ea940b1b4544e648eb62dbd
Sophie LaBelle <sophie.r.labelle@gmail.com>
2023-03-03 10:00:55 -0800

    Create a method to retrive open rooms for a date

--- src/classes/Hotel.js
+++ src/classes/Hotel.js
@@ -4,7 +4,16 @@ class Hotel {
     this.allCustomers = customers;
     this.allRooms = rooms;
     this.openRooms;
+    this.bookedRoomsOnDate;
     this.newBookings;
+  }
+
+  retrieveOpenRoomData(date) {
+    this.bookedRoomsOnDate = this.allBookings.filter(booking => booking.date === date).map(b => b.roomNumber);
+    this.openRooms = this.allRooms.filter(room => !this.bookedRoomsOnDate.includes(room.number));
+  }
+
+  bookNewRoom(book) {
   }
 
   retrieveCustomerBookings(customer) {
@@ -12,20 +21,9 @@ class Hotel {
     return findBookings;
   }
 
-  retiveRoomData() {
-    const findRoom = this.allRooms.map(room => room.number === room.number);
-    return findRoom;
-    // should be able to have an if statement that looks through all rooms and compares it to all the bookings
-    // if a room is not associated with a booking then it is pushed into the open array
-  }
-
-  bookNewRoom() {
-    // should be able to use the open room array and book a room from their with a user and room data and add it to 
-    // add this new room and post request with this new room to the bookings array
-  }
-
-  getTotalSpent(roomData) {
-    this.totalSpent = this.allRooms.reduce((acc, cur) => {
+  getTotalSpent(customer) {
+    // const 
+    this.totalSpent = this.rooms.reduce((acc, cur) => {
       roomData.map(room => {
         if(room.number === cur.roomNumber) {
           acc += room.costPerNight;
